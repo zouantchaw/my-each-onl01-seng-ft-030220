b95294e83b3013fde6598c2e0367b6240f77156b
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-03-11 23:13:12 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
 def my_each(array)
-  
+  if block_given?
+    i = 0
+
+    while i < array.length
+      yield(array[i])
+      i += 1
+    end
+    array
+  else
+    puts "Hey! No block was given!"
+  end
 end
